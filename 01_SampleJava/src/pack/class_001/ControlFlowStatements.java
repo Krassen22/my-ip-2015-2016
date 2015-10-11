d939acf15ad7667b4cdf228170a13c016ad40da3
Krassen22 <krassen22@gmail.com>
2015-10-11 19:44:35 +0300

    If-else example

---
+++ 01_SampleJava/src/pack/class_001/ControlFlowStatements.java
@@ -0,0 +1,21 @@
+package pack.class_001;
+
+public class ControlFlowStatements {
+
+	public static void main(String[] args) {
+		
+		// only 'if'
+		if(true) {
+			System.out.println("called");
+		}
+		
+		// 'if' and 'else'
+		if(3>=2) {
+			System.out.println("3 is greater or equal then 2");
+		} else {
+			System.out.println("3 is less then 2");
+		}
+
+	}
+
+}
