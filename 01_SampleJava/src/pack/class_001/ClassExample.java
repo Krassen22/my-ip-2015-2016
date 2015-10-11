09f180ae7be4a039331cd5819f2725c91221ff37
Krassen22 <krassen22@gmail.com>
2015-10-11 18:58:38 +0300

    Final keyword example && different types error

--- 01_SampleJava/src/pack/class_001/ClassExample.java
+++ 01_SampleJava/src/pack/class_001/ClassExample.java
@@ -5,7 +5,12 @@ public class ClassExample {
 	public static void main(String[] args) {
 		
 		new Room();
-		Room room = new Room();
+		
+		// The keyword 'final' means that reference 'room' can't be changed.
+		final Room room = new Room();
+		
+		// different types error
+		// Wardrobe wardrobe = new Room();
 		
 	}
 	
