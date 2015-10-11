2e52d868768b62518407df533d69908f67ff038c
Krassen22 <krassen22@gmail.com>
2015-10-11 19:23:12 +0300

    Class variables & getters and setters

---
+++ 01_SampleJava/src/pack/class_001/Cube.java
@@ -0,0 +1,29 @@
+package pack.class_001;
+
+public class Cube {
+	
+	private int width;
+	private int length;
+	private int height;
+	
+	// Sourse -> Generate Getters and Setters...
+	public int getWidth() {
+		return width;
+	}
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	public int getLength() {
+		return length;
+	}
+	public void setLength(int length) {
+		this.length = length;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	
+}
---
+++ 01_SampleJava/src/pack/class_001/Minecraft.java
@@ -0,0 +1,30 @@
+package pack.class_001;
+
+public class Minecraft {
+
+	public static void main(String[] args) {
+		
+		int cubeWidth = 4;
+		int cubeLength = 4;
+		int cubeHeight = 4;
+		
+		final Cube cube = new Cube();
+		
+		cube.setWidth(cubeWidth);
+		cube.setLength(cubeLength);
+		cube.setHeight(cubeHeight);
+		
+		System.out.print("Cube width:  ");
+		System.out.println(cube.getWidth());
+		
+		System.out.print("Cube length: ");
+		System.out.println(cube.getLength());
+		
+		System.out.print("Cube height: ");
+		System.out.println(cube.getHeight());
+		
+		System.out.printf("Cube volume: %d", cube.getWidth()*cube.getLength()*cube.getHeight());
+		
+	}
+
+}
