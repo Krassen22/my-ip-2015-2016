4fae0dde48f848426bc356b83a1c28a9ae329877
Krassen22 <krassen22@gmail.com>
2015-11-16 00:10:22 +0200

    Command splitted in two or three(depending on the command) alphabetic strings

--- ip_availability/src/pack002/CommandsExecutor.java
+++ ip_availability/src/pack002/CommandsExecutor.java
@@ -3,11 +3,24 @@ package pack002;
 import java.io.PrintStream;
 import java.util.Map;
 
+import pack002.commands.*;
 import pack002.user.User;
 
 public class CommandsExecutor {
+	private String userName;
+	private String userName2; 
+	private String commandWord;
 	
 	public synchronized void execute(String[] splittedCommand, PrintStream out, Server server, Map<String,User> usersInfo){
-		System.out.println("hello");
+		if(splittedCommand.length == 2){
+			userName = splittedCommand[0];
+			commandWord = splittedCommand[1];
+			
+		}else if(splittedCommand.length == 3 && "info".equals(splittedCommand[1])){
+			userName = splittedCommand[0];
+			userName2 = splittedCommand[2];
+			commandWord = splittedCommand[1];
+			
+		}
 	}
 }
