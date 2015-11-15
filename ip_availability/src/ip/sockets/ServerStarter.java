8178f968c4a397da0ef7c4b345082139fb1d97e2
Krassen22 <krassen22@gmail.com>
2015-11-15 15:05:52 +0200

    Created Server sceleton

---
+++ ip_availability/src/ip/sockets/ServerStarter.java
@@ -0,0 +1,13 @@
+package ip.sockets;
+
+public class ServerStarter {
+	private static final int SERVER_PORT = 31111;
+	
+	public static void main(String[] args) {
+		
+		final Server server = new Server(SERVER_PORT);
+		server.startServer();
+
+	}
+
+}
