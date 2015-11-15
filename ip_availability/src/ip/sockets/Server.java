fb3f65f71cc3ed4a5d4684f1d827c30cb8a0cf8f
Krassen22 <krassen22@gmail.com>
2015-11-15 18:04:43 +0200

    Server socket is immediately closed when stopServer is received

--- ip_availability/src/ip/sockets/Server.java
+++ ip_availability/src/ip/sockets/Server.java
@@ -3,6 +3,7 @@ package ip.sockets;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -11,28 +12,37 @@ public class Server {
 	private final int port;
 	private boolean running;
 	private final List<ClientHandler> clients = Collections.synchronizedList(new LinkedList<ClientHandler>());
+	private ServerSocket serverSocket;
 	
 	public Server(int port){
 		this.port = port;
 	}
 	
 	public void startServer() throws IOException{
-		setRunning();
-		final ServerSocket serverSocket = new ServerSocket(port);
+		
+		final ServerSocket localServerSocket = createServerSocket();
 		while(isRunning()){
-			final Socket socket = serverSocket.accept();
+			final Socket socket;
+			try {
+				socket = localServerSocket.accept();
+			} catch (SocketException e) {
+				if (!localServerSocket.isClosed())
+					throw e;
+				break;
+			}
 			final ClientHandler client = new ClientHandler(this, socket);
 			clients.add(client);
 			new Thread(client).start();
 		}
-		serverSocket.close();
 	}
 	
-	private synchronized void setRunning(){
-		if(running){
+	private synchronized ServerSocket createServerSocket() throws IOException{
+		if(!running){
 			throw new IllegalStateException("Already running");
 		}
 		running = true;
+		serverSocket = new ServerSocket(port);
+		return serverSocket;
 	}
 	
 	private synchronized boolean isRunning(){
@@ -40,7 +50,13 @@ public class Server {
 	}
 	
 	public synchronized void stopServer() throws IOException{
+		if(!running){
+			throw new IllegalStateException("Not running");
+		}
+		
 		running = false;
+		serverSocket.close();
+		serverSocket = null;
 		
 		for (ClientHandler next : clients){
 			next.stopClient();
